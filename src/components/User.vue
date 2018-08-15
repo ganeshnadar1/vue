36a9b2c9ebf1be1349cc09484aab785e2fba4a12
ganeshnadar1 <ganeshnadar.cdmx@gmail.com>
2018-08-15 13:08:35 +0530

    hey i have created vue components

---
+++ src/components/User.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <button v-on:click="changeName">change</button>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail :name="name"></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data: function() {
+            return {
+                name:"ganesh"
+            };
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        },
+        methods:{
+            changeName(){
+                if(this.name == 'ganesh') {
+                    this.name += " Nadar";
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ src/components/UserDetail.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>user name:{{ swapName() }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+
+        props:['name'],
+
+        methods:{
+
+            swapName(){
+
+                return this.name.split("").reverse().join("");
+
+            }
+
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
